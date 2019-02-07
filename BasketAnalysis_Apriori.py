ef81c2633bd6fc39caacd7efac7023fc95eae1e6
osgnn <47410651+osgnn@users.noreply.github.com>
2019-02-07 10:18:12 +0300

    Update BasketAnalysis_Apriori.py
    delete custom file path

--- BasketAnalysis_Apriori.py
+++ BasketAnalysis_Apriori.py
@@ -3,12 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-sepet=pd.read_csv("C:\\Users\\ozgun\\Desktop\\Programlar\\Python\\market.csv",header=None) 
+sepet=pd.read_csv("market.csv",header=None) 
 
 t=[]
 for i in range (0,9835): 
     t.append([str(sepet.values[i,j]) for j in range (0,32)])
-
 
 from apyori import apriori
 kurallar = list(apriori(t,min_support=0.01, min_confidence=0.3, min_lift = 3, min_length=2))
